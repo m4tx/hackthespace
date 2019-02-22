d3359dcabaf19ef474f89b5cb2ddf4a5c5ddf9ee
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-02-22 12:53:19 +0100

    Replace HomeView with Rot13PuzzleView

--- game/puzzles/urls.py
+++ game/puzzles/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from game.puzzles.image.puzzle import ImagePuzzleView
 from game.puzzles.redirect.puzzle import RedirectFailPuzzleView
 from game.puzzles.redirect.puzzle import RedirectPuzzleView
+from game.puzzles.rot13.puzzle import Rot13PuzzleView
 from game.puzzles.terminal.puzzle import TerminalPuzzleView
-from game.views import HomeView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='rot13'),
+    path('', Rot13PuzzleView.as_view(), name='rot13'),
     path('toomuchwant/', ImagePuzzleView.as_view(), name='image'),
     path('h4x.sh/', TerminalPuzzleView.as_view(), name='terminal'),
 
