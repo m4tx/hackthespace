485dad0c7710beb588d182272559853ec2c92afb
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-03-01 20:05:50 +0100

    Update add_puzzle

--- game/management/commands/add_puzzle.py
+++ game/management/commands/add_puzzle.py
@@ -1,5 +1,6 @@
 import os
 import re
+from django.core.management import call_command
 
 from django.core.management.base import BaseCommand, CommandError
 
@@ -15,19 +16,21 @@ TEMPLATE_CONTENT = '''{% extends 'base.html' %}
 {% endblock %}
 '''
 
-PUZZLE_PY_CONTENT = '''from django.views.generic import TemplateView
+VIEWS_PY_CONTENT = '''from django.views.generic import TemplateView
 
 
 class {title_name}PuzzleView(TemplateView):
     template_name = 'puzzles/{name}.html'
 '''
 
-URL_REGEX = r'^(\nurlpatterns = \[.+)\]$'
-URL_SUBST = (
-    'from game.puzzles.{name}.puzzle import {title_name}PuzzleView\n'
-    '\\1'
-    '    path(\'{name}/\', {title_name}PuzzleView.as_view(), '
-    'name=\'{name}\'),\n]')
+URLS_PY_CONTENT = '''from django.urls import path
+
+from {name}.views import {title_name}PuzzleView
+
+urlpatterns = [
+    path('', {title_name}PuzzleView.as_view(), name='{name}'),
+]
+'''
 
 SETTINGS_REGEX = r'^(PUZZLE_ORDER = \[.+?)\]'
 SETTINGS_SUBST = '\\1    \'{name}\',\\n]'
@@ -44,46 +47,44 @@ class Command(BaseCommand):
         name: str = options.pop('name').lower()
         title_name = name.title().replace('_', '')
 
+        call_command('startapp', name)
         self.__create_files(name, title_name)
-        self.__add_to_urls(name, title_name)
         self.__add_to_order(name, title_name)
 
     @staticmethod
-    def __get_app_root():
-        return os.path.dirname(os.path.dirname(os.path.dirname(
-            os.path.abspath(__file__))))
+    def __get_project_root():
+        return os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(
+            os.path.abspath(__file__)))))
+
+    def __get_app_root(self, name: str):
+        return os.path.join(self.__get_project_root(), name)
 
     def __create_files(self, name, title_name):
-        app_root = self.__get_app_root()
-        templates_root = os.path.join(app_root, 'templates', 'puzzles')
-        puzzles_root = os.path.join(app_root, 'puzzles')
-        puzzle_path = os.path.join(puzzles_root, name)
+        app_root = self.__get_app_root(name)
+        templates_root = os.path.join(app_root, 'templates')
+        os.makedirs(templates_root)
 
-        self.__create_puzzle_dir(puzzle_path)
-        self.__create_puzzle_files(puzzle_path, name, title_name)
+        self.__create_views_file(app_root, name, title_name)
         self.__create_template_file(templates_root, name)
+        self.__create_urls_file(app_root, name, title_name)
 
     @staticmethod
-    def __create_puzzle_dir(puzzle_path):
-        try:
-            os.makedirs(puzzle_path)
-        except FileExistsError:
-            raise CommandError('Puzzle with this name already exists!')
-
-    @staticmethod
-    def __create_puzzle_files(puzzle_path, name, title_name):
-        init_py_path = os.path.join(puzzle_path, '__init__.py')
-        with open(init_py_path, 'w'):
-            pass
-        puzzle_py_path = os.path.join(puzzle_path, 'puzzle.py')
+    def __create_views_file(puzzle_path, name, title_name):
+        puzzle_py_path = os.path.join(puzzle_path, 'views.py')
         with open(puzzle_py_path, 'w') as f:
-            f.write(PUZZLE_PY_CONTENT.format(name=name, title_name=title_name))
+            f.write(VIEWS_PY_CONTENT.format(name=name, title_name=title_name))
 
     @staticmethod
     def __create_template_file(templates_root, name):
         template_path = os.path.join(templates_root, '{}.html'.format(name))
         with open(template_path, 'w') as f:
             f.write(TEMPLATE_CONTENT)
+
+    @staticmethod
+    def __create_urls_file(templates_root, name, title_name):
+        urls_path = os.path.join(templates_root, 'urls.py')
+        with open(urls_path, 'w') as f:
+            f.write(URLS_PY_CONTENT.format(name=name, title_name=title_name))
 
     @staticmethod
     def __apply_regex_to_file(path, regex, subst, name, title_name):
@@ -95,13 +96,8 @@ class Command(BaseCommand):
             f.seek(0)
             f.write(new_contents)
 
-    def __add_to_urls(self, name, title_name):
-        urls_path = os.path.join(self.__get_app_root(), 'puzzles', 'urls.py')
-        self.__apply_regex_to_file(urls_path, URL_REGEX, URL_SUBST,
-                                   name, title_name)
-
     def __add_to_order(self, name, title_name):
-        urls_path = os.path.join(self.__get_app_root(),
-                                 '..', 'stronghold', 'settings', 'base.py')
+        urls_path = os.path.join(self.__get_project_root(),
+                                 'stronghold', 'settings', 'base.py')
         self.__apply_regex_to_file(urls_path, SETTINGS_REGEX, SETTINGS_SUBST,
                                    name, title_name)
