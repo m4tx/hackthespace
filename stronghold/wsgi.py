68ed3e156caafd19ed8b6e8dea179b3acfcbc7ae
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-03-05 18:09:43 +0100

    Add production settings example

--- stronghold/wsgi.py
+++ stronghold/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stronghold.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'stronghold.settings.production_settings')
 
 application = get_wsgi_application()
