99a3905ce4dc533b934cee3e8b684483ac4c7741
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-03-20 00:27:44 +0100

    Add username to login puzzle, use pirate tongue

--- login/views.py
+++ login/views.py
@@ -6,6 +6,7 @@ from game.puzzle_order import get_next_puzzle_url
 
 
 class LoginForm(forms.Form):
+    username = forms.CharField(max_length=50)
     password = forms.CharField(max_length=50)
 
 
@@ -15,15 +16,18 @@ class LoginPuzzleView(FormView):
     form_class = LoginForm
     success_url = get_next_puzzle_url('login')
 
-    password = 'neverhood'
+    username = 'Cannonbeam'
+    password = 'starjammer'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['username'] = self.username
         context['password'] = self.password
         return context
 
     def form_valid(self, form):
-        if form.cleaned_data['password'] != self.password:
+        if (form.cleaned_data['password'] != self.password or
+                form.cleaned_data['username'] != self.username):
             form.add_error('password', 'The password you provided is invalid')
             return self.form_invalid(form)
 
