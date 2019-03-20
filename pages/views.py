16daa7f0d9d5631832b9febb8ceb56725cf8b14b
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-03-20 16:36:46 +0100

    Move page lookup form object to forms.py

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,8 @@
-from django import forms
 from django.views.generic import ListView
 
 from game.decorators import puzzle
+from pages.forms import SearchForm
 from pages.models import Puzzle
-
-
-class SearchForm(forms.Form):
-    query = forms.CharField(max_length=100)
 
 
 @puzzle
