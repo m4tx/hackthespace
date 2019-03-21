fb75d2826825d12ab5bf238e53d035749adbbb70
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-03-21 03:47:58 +0100

    Update keypad URL

--- keypad/urls.py
+++ keypad/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from keypad.views import KeypadPuzzleView
 
 urlpatterns = [
-    path('keypad/', KeypadPuzzleView.as_view(), name='keypad'),
+    path('doorkeypad/', KeypadPuzzleView.as_view(), name='keypad'),
 ]
