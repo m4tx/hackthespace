9b7e9d8ccc72226037c598cad711f0c1258b5689
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-03-21 03:44:33 +0100

    Use @puzzle for keypad

--- keypad/views.py
+++ keypad/views.py
@@ -1,12 +1,14 @@
 from django.views.generic import FormView
 
+from game.decorators import puzzle
 from game.puzzle_order import get_next_puzzle_url
 from keypad.forms import KeypadForm
 
 
+@puzzle
 class KeypadPuzzleView(FormView):
     template_name = 'keypad/puzzle.html'
     form_class = KeypadForm
 
     def get_success_url(self):
-        return get_next_puzzle_url('keypad')
+        return get_next_puzzle_url(KeypadPuzzleView.puzzle_name)
