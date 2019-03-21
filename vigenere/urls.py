60edbe2233cfaffe53978ebc37fd4d61b619d4ef
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-03-21 02:25:49 +0100

    Update vigenere URL

--- vigenere/urls.py
+++ vigenere/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from vigenere.views import VigenerePuzzleView
 
 urlpatterns = [
-    path('vinegar/', VigenerePuzzleView.as_view(), name='vigenere'),
+    path('dramaticvinegar/', VigenerePuzzleView.as_view(), name='vigenere'),
 ]
