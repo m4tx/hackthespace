cc0c9e308227327479724b5950c34e9ea4914428
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-03-22 13:11:29 +0100

    Catch SQL errors in pagelookup

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from django.db import OperationalError
 from django.views.generic import ListView, TemplateView
 
 from game.decorators import puzzle, hidden_puzzle, requires_email
@@ -16,11 +17,22 @@ class PagesPuzzleView(ListView):
     def get_queryset(self):
         form = self.form_class(self.request.GET)
         if form.is_valid():
-            return Puzzle.objects.raw(
+            query = Puzzle.objects.raw(
                 'SELECT * from pages_puzzle WHERE url = \'{}\''.format(
                     form.cleaned_data['query']))
+            len(query)  # Force evaluate the query
+            return query
 
         return Puzzle.objects.none()
+
+    def get(self, request, *args, **kwargs):
+        try:
+            return super().get(request, *args, **kwargs)
+        except OperationalError:
+            self.object_list = []
+            context = self.get_context_data()
+            context['query_error'] = True
+            return self.render_to_response(context)
 
 
 @hidden_puzzle
